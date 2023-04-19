cfc8ab9c2d18e173ea965514f930a793dc84cf21
FelipeAp1903 <131301360+FelipeAp1903@users.noreply.github.com>
2023-04-19 17:44:26 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double x;
+            double y;
+            double p;
+
+            Console.Write("coloque o valor de x:");
+            x = double.Parse(Console.ReadLine());
+            Console.Write("coloque o valor de y:");
+            y = double.Parse(Console.ReadLine());
+            p = Math.Pow(x, y);
+            Console.WriteLine("a potencia é: {0}", p);
+        }
+    }
+}
